963a18aba891598378b40954bc651431f8cb720e
Nancy Ramírez <nancylissethramirezservellon@gmail.com>
2023-05-01 12:05:22 -0600

    a

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,8 @@ module.exports = {
   theme: {
     extend: {
         colors:{
-          transparent: 'transparent',
-          current: 'currentColor',
-          aFuerte: '#0A1128',
-          aFuerte2: '#001F54',
-          aFuerte3: '#034078',
-          aFuerte4: '#1282A2',
-          bCasi: '#FEFCFB'
+          "dark-purple": "#081A51",
+          "light-white": "rgba(255,255,255, 0.18)"
       },
     },
   },
