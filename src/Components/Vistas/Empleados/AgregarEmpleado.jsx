39adb409fa887aeb6de63a323cf891453f315dc7
Nancy Ramírez <nancylissethramirezservellon@gmail.com>
2023-05-09 00:30:42 -0600

    mascaras

--- src/Components/Vistas/Empleados/AgregarEmpleado.jsx
+++ src/Components/Vistas/Empleados/AgregarEmpleado.jsx
@@ -2,8 +2,35 @@ import { Link } from "react-router-dom";
 import { Aside } from "../../Componentes/Aside";
 import { Navbar } from "../../Componentes/NavBar";
 import { FiArrowLeft } from "react-icons/fi";
+import "../../Styles/agregar.css";
+import { useEffect } from "react";
+import $ from 'jquery';
+import 'jquery-mask-plugin';
 
 export const AgregarEmpleados = () => {
+
+  {/*Mascaras */}
+  useEffect(() => {
+
+    $("#salario").change(function() {
+      var valor = $(this).val();
+      if (valor.indexOf("*") !== -1) {
+        var punto = ".";
+        valor = valor.replace("*", punto);
+        $(this).val(valor);
+      }
+    });
+
+    $(document).ready(function() {
+      $("#telefono").mask("0000-0000");
+      $("#nit").mask("0000-000000-000-0");
+      $("#dui").mask("00000000-0");
+      $("#isss").mask("000000000");
+      $("#afp").mask("00000000000");
+      $("#salario").mask("0000000.00", {reverse: true});
+    });
+  }, []);
+
   return (
     <div className="flex">
       <Aside />
@@ -41,7 +68,7 @@ export const AgregarEmpleados = () => {
                         type="text"
                         id="nombres"
                         name="nombres"
-                        class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                        class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"  pattern="[A-Za-z]*"
                       />
                     </div>
                     {/*Apellidos */}
@@ -56,7 +83,7 @@ export const AgregarEmpleados = () => {
                         type="text"
                         id="apellidos"
                         name="apellidos"
-                        class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                        class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" pattern="[A-Za-z]*"
                       />
                     </div>
                   </div>
@@ -78,18 +105,22 @@ export const AgregarEmpleados = () => {
                         class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                       />
                     </div>
-                    {/*DUI */}
-                    <div className="xl:w-1/2 md:w-1/2 w-full px-2">
-                      <label for="dui" class="leading-7 text-sm text-gray-600">
-                        DUI
+                    {/*Telefono */}
+                    <div class="xl:w-1/2 md:w-1/2 w-full px-2">
+                      <label
+                        for="telefono"
+                        class="leading-7 text-sm text-gray-600"
+                      >
+                        Teléfono
                       </label>
                       <input
-                        type="number"
-                        id="dui"
-                        name="dui"
-                        class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                        type="text"
+                        id="telefono"
+                        name="telefono"
+                        class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" inputmode="Numeric" 
                       />
                     </div>
+                    
                   </div>
 
                   {/*Linea 3 */}
@@ -100,30 +131,55 @@ export const AgregarEmpleados = () => {
                         NIT
                       </label>
                       <input
-                        type="number"
+                        type="text"
                         id="nit"
                         name="nit"
                         class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                       />
                     </div>
-                    {/*Telefono */}
-                    <div class="xl:w-1/2 md:w-1/2 w-full px-2">
-                      <label
-                        for="telefono"
-                        class="leading-7 text-sm text-gray-600"
-                      >
-                        Teléfono
+                    {/*DUI */}
+                    <div className="xl:w-1/2 md:w-1/2 w-full px-2">
+                      <label for="dui" class="leading-7 text-sm text-gray-600">
+                        DUI
                       </label>
                       <input
-                        type="number"
-                        id="telefono"
-                        name="telefono"
+                        type="text"
+                        id="dui"
+                        name="dui"
                         class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                       />
                     </div>
                   </div>
 
-                  {/*Linea 4 */}
+                   {/*Linea 4 */}
+                   <div className="flex flex-wrap px-10">
+                    {/*ISSS */}
+                    <div className="xl:w-1/2 md:w-1/2 w-full px-2">
+                      <label for="isss" class="leading-7 text-sm text-gray-600">
+                        ISSS
+                      </label>
+                      <input
+                        type="text"
+                        id="isss"
+                        name="isss"
+                        class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" placeholder="Si no posee ISSS dejar vacío" 
+                      />
+                    </div>
+                    {/*AFP */}
+                    <div className="xl:w-1/2 md:w-1/2 w-full px-2">
+                      <label for="afp" class="leading-7 text-sm text-gray-600">
+                        AFP
+                      </label>
+                      <input
+                        type="text"
+                        id="afp"
+                        name="afp"
+                        class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" placeholder="Si no posee AFP dejar vacío"
+                      />
+                    </div>
+                  </div>
+
+                  {/*Linea 5 */}
                   <div className="flex flex-wrap px-10">
                     {/*Dirección */}
                     <div className="w-full px-2">
@@ -142,7 +198,7 @@ export const AgregarEmpleados = () => {
                     </div>
                   </div>
 
-                  {/*Linea 5 */}
+                  {/*Linea 6 */}
                   <div className="flex flex-wrap px-10">
                     {/*Correo electrónico*/}
                     <div className="xl:w-1/2 md:w-1/2 w-full px-2">
@@ -181,7 +237,7 @@ export const AgregarEmpleados = () => {
                     </div>
                   </div>
 
-                  {/*Linea 6 */}
+                  {/*Linea 7 */}
                   <div className="flex flex-wrap px-10">
                     {/*Sexo */}
                     <div class="xl:w-1/2 md:w-1/2 w-full px-2">
@@ -204,7 +260,7 @@ export const AgregarEmpleados = () => {
 
                 {/*Parte 2 */}
                 <div class="bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10">
-                  {/*Linea 7 */}
+                  {/*Linea 8 */}
                   <div className="flex flex-wrap px-10">
                     {/*Salario */}
                     <div className="xl:w-1/2 md:w-1/2 w-full px-2">
@@ -212,15 +268,19 @@ export const AgregarEmpleados = () => {
                         for="salario"
                         class="leading-7 text-sm text-gray-600"
                       >
-                        Nombres
+                        Salario
                       </label>
-                      <input
-                        type=""
-                        id="salario"
-                        name="salario"
-                        class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                        placeholder="$"
-                      />
+                      <div className="relative md:content-center">
+                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                      $
+                    </div>
+                    <input
+                      type="text"
+                      id="salario"
+                      className="block p-2 pl-10 w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                      
+                    />
+                  </div>
                     </div>
                     {/*Tipo de pago */}
                     <div className="xl:w-1/2 md:w-1/2 w-full px-2">
@@ -242,7 +302,7 @@ export const AgregarEmpleados = () => {
                     </div>
                   </div>
 
-                  {/*Linea 8 */}
+                  {/*Linea 9 */}
                   <div className="flex flex-wrap px-10">
                     {/*Departamento*/}
                     <div class="xl:w-1/2 md:w-1/2 w-full px-2">
@@ -276,7 +336,7 @@ export const AgregarEmpleados = () => {
                     </div>
                   </div>
 
-                  {/*Linea 9 */}
+                  {/*Linea 10 */}
                   <div className="flex flex-wrap px-10">
                     {/*Tipo contrato*/}
                     <div class="xl:w-1/2 md:w-1/2 w-full px-2">
